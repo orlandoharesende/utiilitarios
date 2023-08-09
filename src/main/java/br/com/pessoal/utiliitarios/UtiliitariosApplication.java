fdc72d01bc29c592a77a34656ee99f119a6f0a39
Orlando Resende <orlando.resende@tjdft.jus.br>
2023-08-09 00:15:24 -0300

    Commit inicial

---
+++ src/main/java/br/com/pessoal/utiliitarios/UtiliitariosApplication.java
@@ -0,0 +1,13 @@
+package br.com.pessoal.utiliitarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtiliitariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtiliitariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/pessoal/utiliitarios/UtiliitariosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pessoal.utiliitarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtiliitariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
